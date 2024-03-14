3b35e66a24c77159ccfea650b33b1f5a522a794e
Dmitry Stukateev <Dmitry.Stukateev@yandex.ru>
2024-03-14 16:11:36 +0300

    Version 0.0.1 change usersCallBack.js add type and description value

--- controllers/usersCallBack.js
+++ controllers/usersCallBack.js
@@ -10,17 +10,20 @@ const bot = new TelegramBot(API_KEY_BOT, {
 
 
 });
-async function sendMessageInTelegram(name, number) {
-    await bot.sendMessage(CHAT_ID, `<b>Пришла заявка.</b> Имя: <u>${name}</u> Телефон: <u>${number}</u> `);
+async function sendMessageInTelegram(name, number, type, description) {
+    await bot.sendMessage(CHAT_ID, `Пришла заявка. Имя: ${name} 
+    Телефон: ${number} 
+    ${type} 
+    ${description}`);
 }
 
 const createCallBack = (req, res, next) => {
     const {
-        name, number
+        name, number, type, description
     } = req.body;
 
     return UsersCallBack.create({
-        name, number
+        name, number, type, description,
     })
         .then((people) => {
             res.status(201).send(people)
